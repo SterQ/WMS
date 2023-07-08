0b542424a038af91ea46c351642363f73e2c55f2
SterQ <44976789+SterQ@users.noreply.github.com>
2023-07-08 23:19:00 +0200

    Add files via upload

---
+++ passport-config.js
@@ -0,0 +1,45 @@
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const bcrypt = require("bcrypt");
+const User = require("./userModel");
+
+passport.use(
+  new LocalStrategy(
+    { usernameField: "email" },
+    async (email, password, done) => {
+      try {
+        const user = await User.findOne({ email: email });
+        if (!user) {
+          return done(null, false, {
+            message: "Nieprawidłowy email lub hasło.",
+          });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (isMatch) {
+          return done(null, user);
+        } else {
+          return done(null, false, {
+            message: "Nieprawidłowy email lub hasło.",
+          });
+        }
+      } catch (error) {
+        return done(error);
+      }
+    }
+  )
+);
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (error) {
+    done(error);
+  }
+});
+
+module.exports = passport;
